0a8acc750ab8d6af324c7f2d57582c9427fbc5a4
prowin99worlds <pravin.salonshark@gmail.com>
2025-07-28 17:18:09 +0530

    admin dashboard login created

--- django_app/admin.py
+++ django_app/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# django_app/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import YourModel
+
+admin.site.register(YourModel)
--- django_app/urls.py
+++ django_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
