06e8c8473020f382a592e096a6de0b77c542f7f7
prowin99worlds <pravin.salonshark@gmail.com>
2025-07-28 13:18:46 +0530

    render host settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$#)0^0=wcyz2c+)mc^2v&m)w-s7ai=#--0a3rzt-m&qv)a+l=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
