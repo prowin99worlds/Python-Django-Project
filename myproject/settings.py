623f84b48a3c98a90364de9bc8fae96b26910a7b
prowin99worlds <pravin.salonshark@gmail.com>
2025-07-28 17:04:51 +0530

    fixing css js static files again

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    'C:/Users/ADMIN/Documents/New Django Project/soft-ui-design-system-main/assets'
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
