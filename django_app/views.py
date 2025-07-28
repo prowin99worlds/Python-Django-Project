936095d6b8a49da60c82eb47bd0a2a40f286d3ea
prowin99worlds <pravin.salonshark@gmail.com>
2025-07-28 15:50:48 +0530

    finally deployed on render after renderyaml file setting

--- django_app/views.py
+++ django_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to Django App!")
+    return render(request, 'index.html')
